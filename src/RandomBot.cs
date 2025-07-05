ecb5106bcc6575a50636f6c5cb280a50194ff8d1
FormerlyCB5K <bas_l@ncintegration.com>
2025-07-05 14:48:59 -0400

    Add files via upload

---
+++ src/RandomBot.cs
@@ -0,0 +1,24 @@
+﻿using ScriptsOfTribute;
+using ScriptsOfTribute.AI;
+using ScriptsOfTribute.Board;
+using ScriptsOfTribute.Serializers;
+
+namespace Bots;
+
+public class RandomBot : AI
+{
+    private readonly SeededRandom rng = new(123);
+
+    public override PatronId SelectPatron(List<PatronId> availablePatrons, int round)
+        => availablePatrons.PickRandom(rng);
+
+    public override Move Play(GameState gameState, List<Move> possibleMoves, TimeSpan remainingTime)
+    {
+        var move = possibleMoves.PickRandom(rng);
+        return move;
+    }
+
+    public override void GameEnd(EndGameState state, FullGameState? finalBoardState)
+    {
+    }
+}
